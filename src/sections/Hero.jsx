490063ca8f4cab1f7e21a49adaa19edf06295a3b
Miles Wildmore <mileswildmore18@gmail.com>
2025-01-15 11:04:04 -0500

    Added a Hero section for the app JSX and added the introduction to the hero section introducing myself as well as changing the website name to my portfolio.

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,12 @@
+const Hero = () => {
+    return (
+        // Add hero section with blue border
+        <section className="min-h-screen border-2 border-blue-500 w-full flex flex-col relative">Hero
+        <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+            <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">Hello, my name is Miles Wildmore<span className="waving-hand">👋</span></p>
+
+        </div>
+        </section>
+    )
+}
+export default Hero
