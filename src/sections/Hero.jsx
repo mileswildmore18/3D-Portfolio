6e60ea7e79107df516590076df96228acd9623cc
Miles Wildmore <mileswildmore18@gmail.com>
2025-01-15 11:11:36 -0500

    Installed Three Js packages along with responsiveness from react and leva simplifying process of adding real time controls to the app. Also added a secondary title for the hero section.

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -5,7 +5,11 @@ const Hero = () => {
         <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
             <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">Hello, my name is Miles Wildmore<span className="waving-hand">👋</span></p>
 
+            <p className="hero_tag text-gray_gradient">Building Products and Brands</p>
         </div>
+            <div className="w-full h-full absolute inset-0">
+
+            </div>
         </section>
     )
 }
