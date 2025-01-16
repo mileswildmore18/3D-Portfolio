53e19d4122fd511095a4b7b696dc14d6560afe36
Miles Wildmore <mileswildmore18@gmail.com>
2025-01-16 13:44:38 -0500

    Added prop types to the eslint file line 34 and corrected line 6 to reactThree. Also changed the position of the React logo.

--- src/components/ReactLogo.jsx
+++ src/components/ReactLogo.jsx
@@ -15,7 +15,7 @@ const ReactLogo = (props) => {
                     material={materials['Material.002']}
                     position={[0, 0.079, 0.181]}
                     rotation={[0, 0, -Math.PI / 2]}
-                    scale={[0.39, 0.39, 0.5]}
+                    scale={[0.392, 0.392, 0.527]}
                 />
             </group>
         </Float>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -23,7 +23,7 @@ const Hero = () => {
     const sizes = calculateSizes(isSmall, isMobile, isTablet)
     return (
         // Add hero section with blue border
-        <section className="min-h-screen border-2 border-blue-500 w-full flex flex-col relative">Hero
+        <section className="min-h-screen w-full flex flex-col relative" id="home">
             <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
                 <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">Hello, my name
                     is Miles Wildmore<span className="waving-hand">👋</span></p>
