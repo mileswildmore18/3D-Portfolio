8b6c649d674b64ce831c8933f22e0f57f8e12f27
Miles Wildmore <mileswildmore18@gmail.com>
2025-01-16 14:36:47 -0500

    Added a button to contact the main person with some animations to make it appealing for users to click on.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
-
-const Button = () => {
+// Add button
+const Button = ({name, isBeam = false, containerClass}) => {
     return (
-        <div>Button</div>
+        <button className={`btn $${containerClass}`}>
+            {isBeam && (
+                <span className='relative flex h-3 w-3'>
+                    <span className="btn-ping"/>
+                    <span className="btn-ping_dot"/>
+                </span>
+            )}
+            {name}
+        </button>
     )
 }
 export default Button
