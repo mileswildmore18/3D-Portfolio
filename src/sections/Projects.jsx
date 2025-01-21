d1dee4f674d4fdcaea05201bb771fa41500209b8
Miles Wildmore <mileswildmore18@gmail.com>
2025-01-21 11:24:48 -0500

    Added orbit controls for the 3d model added to the work section as well as adding the necessary glb code for the 3d model to work. Next will be to animation the camera motion of the 3d model for each project. Also added the new DemoComputer jsx file to import the code to the Projects section.

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,5 +1,9 @@
 import {myProjects} from "../constants/index.js";
-import {useState} from "react";
+import {Suspense, useState} from "react";
+import {Canvas} from "@react-three/fiber";
+import {Center} from "@react-three/drei";
+import CanvasLoader from "../components/CanvasLoader.jsx";
+import DemoComputer from "../components/DemoComputer.jsx";
 
 // Add amount of projects and current project
 const projectCount = myProjects.length;
@@ -60,6 +64,7 @@ const Projects = () => {
                             <img src="/assets/arrow-up.png" alt="arrow" className="w-3 h-3" />
                         </a>
                     </div>
+                    {/* Add navigation arrows*/}
                     <div className="flex justify-between items-center mt-7">
                         <button className="arrow-btn" onClick={() => handleNavigation('previous')}>
                             <img src="/assets/left-arrow.png" alt="left arrow" className="w-4 h-4"/>
@@ -68,6 +73,20 @@ const Projects = () => {
                             <img src="/assets/right-arrow.png" alt="right arrow" className="w-4 h-4"/>
                         </button>
                     </div>
+                </div>
+
+                <div className="border border-black-300 bg-black-200 rounded-lg h-96 md:h-full">
+                    <Canvas>
+                        <ambientLight intensity={1} />
+                        <directionalLight position={[10, 10, 5]} />
+                        <Center>
+                            <Suspense fallback={<CanvasLoader />}>
+                                <group scale={2} position={[0, -3, 0]} rotation={[0, -0.1, 0]}>
+                                <DemoComputer />
+                                </group>
+                        </Suspense>
+                        </Center>
+                    </Canvas>
                 </div>
             </div>
         </section>
