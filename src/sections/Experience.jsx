8a99a62316c4fe77c74ffc02c6ada93226fc9b57
Miles Wildmore <mileswildmore18@gmail.com>
2025-01-27 11:13:28 -0500

    Added an avatar onto the work experience based on me on the work I have done. Used some other websites for help wth adding animation that is coming next. Also added the Developer section for the avatar as well as added all the necessary code needed to make the avatar appear on the portfolio. Next will be to animate the avatar based on where the user hovers over work experiences I had.

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, {Suspense} from 'react'
 import {Canvas} from "@react-three/fiber";
 import {workExperiences} from "../constants/index.js";
+import {OrbitControls} from "@react-three/drei";
+import CanvasLoader from "../components/CanvasLoader.jsx";
+import Developer from "../components/Developer.jsx";
 
 const Experience = () => {
     return (
@@ -13,7 +16,19 @@ const Experience = () => {
                 <div className="work-container">
                     <div className="work-canvas">
                     <Canvas>
-
+                        {/* Add ambient light intensity to the avatar*/}
+                        <ambientLight intensity={7} />
+                        {/* Add light angles and the shadow effects for avatar*/}
+                        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+                        {/* Add direction of where the light is coming from*/}
+                        <directionalLight position={[10, 10, 10]} />
+                        {/* Add orbit controls around the avatar and avoid going past the bottom of the avatar*/}
+                        <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} />
+                        {/* Add canvas loader for the avatar*/}
+                        <Suspense fallback={<CanvasLoader />}>
+                        {/*    Add avatar    */}
+                            <Developer position-y={-3}/>
+                        </Suspense>
                     </Canvas>
                     </div>
                     {/* Add work content*/}
